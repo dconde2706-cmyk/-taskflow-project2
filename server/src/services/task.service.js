d3248e7d0220cb7461f864c28e9877924fcd836a
dconde2706-cmyk <dconde2706@gmail.com>
2026-03-24 17:12:25 +0100

    Update task.service.js

--- server/src/services/task.service.js
+++ server/src/services/task.service.js
@@ -1,1 +1,34 @@
+let tasks = [];
+let currentId = 1;
 
+const obtenerTodas = () => {
+  return tasks;
+};
+
+const crearTarea = ({ titulo, prioridad }) => {
+  const nueva = {
+    id: currentId++,
+    titulo,
+    prioridad,
+    completado: false
+  };
+
+  tasks.push(nueva);
+  return nueva;
+};
+
+const eliminarTarea = (id) => {
+  const index = tasks.findIndex(t => t.id === id);
+
+  if (index === -1) {
+    throw new Error('NOT_FOUND');
+  }
+
+  tasks.splice(index, 1);
+};
+
+module.exports = {
+  obtenerTodas,
+  crearTarea,
+  eliminarTarea
+};
