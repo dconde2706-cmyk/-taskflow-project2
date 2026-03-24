52c7e06c6e2437f92150ae2612a5806a9f20eee6
dconde2706-cmyk <dconde2706@gmail.com>
2026-03-24 18:26:30 +0100

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const cors = require('cors');
+const { PORT } = require('./config/env');
+
+const taskRoutes = require('./routes/task.routes');
+const logger = require('./middlewares/logger');
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
 app.get('/', (req, res) => {
-  res.send('Servidor funcionando');
+  res.send('API TaskFlow funcionando');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor activo');
+app.use('/api/v1/tasks', taskRoutes);
+
+app.use((err, req, res, next) => {
+  if (err.message === 'NOT_FOUND') {
+    return res.status(404).json({
+      error: 'Tarea no encontrada'
+    });
+  }
+
+  console.error(err);
+
+  res.status(500).json({
+    error: 'Error interno del servidor'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
