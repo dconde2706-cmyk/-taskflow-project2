91cc3058d72ed4fa47e6bf5b21b613adf91a594c
dconde2706-cmyk <dconde2706@gmail.com>
2026-04-28 13:01:11 +0200

    Update task.routes.js

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -4,6 +4,8 @@ const controller = require('../controllers/task.controller');
 
 router.get('/', controller.obtenerTodas);
 router.post('/', controller.crearTarea);
+router.patch('/complete-all', controller.completarTodas); 
+router.delete('/completed', controller.eliminarCompletadas); 
 router.patch('/:id', controller.actualizarEstado);
 router.delete('/:id', controller.eliminarTarea);
 
